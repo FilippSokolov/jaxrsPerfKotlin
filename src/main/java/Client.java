d3f95de20d47f0d2a78c5a7d12ce039c9ddd3799
FilippSokolov <Filipp.Sokolov@jetbrains.com>
2020-03-13 18:05:34 +0300

    zzz

--- src/main/java/Client.java
+++ src/main/java/Client.java
@@ -1,14 +1,13 @@
 
 
 
+import okhttp3.HttpUrl;
+
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.client.Entity;
 import javax.ws.rs.client.WebTarget;
-import javax.ws.rs.core.Application;
-import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Request;
-import javax.ws.rs.core.Response;
+import javax.ws.rs.core.*;
 import java.net.MalformedURLException;
 import java.net.URL;
 
@@ -17,10 +16,7 @@ import java.net.URL;
 public class Client extends Application {
     public void getEndpointTest1() throws MalformedURLException {
 
-        javax.ws.rs.client.Client client = ClientBuilder.newClient();
-        String name = client.target("http://localhost/helloExtended/ext8/versions")
-                .request(MediaType.TEXT_PLAIN)
-                .get(String.class);
+        HttpUrl aseUrl1 = HttpUrl.get("https://localhost:8080/");
     }
 
 
